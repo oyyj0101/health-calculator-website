2bbddb028647b46684d90193c6d9de01b3ecc4ba
LongShaSalad <b1a2i3j4i5u6@gmail.com>
2026-05-11 15:29:25 +0800

    Update Offline健康數據計算器.js
    圖標路徑更新

--- Offline健康數據計算器.js
+++ Offline健康數據計算器.js
@@ -4,7 +4,7 @@ const cacheFiles = [
   './index.html',
   './健康數據計算器.css', // 換成你的 CSS 檔名
   './健康數據計算器.js', // 換成你的 JS 檔名
-  './192icon.png'
+  './健康數據計算器image/192icon.png'
 ];
 
 self.addEventListener('install', e => {
