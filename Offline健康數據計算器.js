ad854e7fc6353c35313eb2fda84dd235b8e7e7ec
LongShaSalad <b1a2i3j4i5u6@gmail.com>
2026-05-11 15:14:22 +0800

    software download

---
+++ Offline健康數據計算器.js
@@ -0,0 +1,16 @@
+const cacheName = 'v1';
+const cacheFiles = [
+  './',
+  './index.html',
+  './健康數據計算器.css', // 換成你的 CSS 檔名
+  './健康數據計算器.js', // 換成你的 JS 檔名
+  './192icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(cacheFiles)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
